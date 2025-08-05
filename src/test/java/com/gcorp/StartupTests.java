5d036685953b12a22e73f29a4e15c74f9068d2b6
George Matos <georgemattos@gmail.com>
2025-08-05 18:51:09 -0300

    first steps with java and springboot 3.4.0

---
+++ src/test/java/com/gcorp/StartupTests.java
@@ -0,0 +1,13 @@
+package com.gcorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
